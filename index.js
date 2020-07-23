b5b163279b338af9fe8e5cdc75f9749b524dead8
zbiogg <tuikhongnhotk@gmail.com>
2020-07-23 23:42:27 +0700

    master

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var server = require("http").createServer(app);
+var server = require("https").createServer(app);
 var io = require("socket.io").listen(server);
 var fs = require("fs");
 server.listen(process.env.PORT || 3000);
